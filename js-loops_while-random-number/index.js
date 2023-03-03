bc8ca7c9f7be7b8f8d2b297c6bedd0ff4f37b78d
Fredrik Wictorsson <wictorsson@gmail.com>
2023-03-03 16:21:59 +0100

    solve while

---
+++ js-loops_while-random-number/index.js
@@ -0,0 +1,19 @@
+console.clear();
+
+// `while` loop
+
+let number = 0;
+let count = 0;
+
+// --v-- write/change code here --v--
+
+while (number <= 0.9) {
+  number = Math.random();
+  console.log(number);
+  count++;
+}
+// --^-- write/change code here --^--
+
+console.log(
+  `The number that ended the loop was ${number} and it took ${count} rounds to do this!`
+);
