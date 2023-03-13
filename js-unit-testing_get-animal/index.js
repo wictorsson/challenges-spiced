066cd393eab3c7edbb6cc9cee0ee842b78897e8b
Fredrik Wictorsson <wictorsson@gmail.com>
2023-03-13 14:58:25 +0100

    solve unit test1

---
+++ js-unit-testing_get-animal/index.js
@@ -0,0 +1,8 @@
+export function getAnimal(animals) {
+  if (animals === "cats") {
+    return "I totally love cats!";
+  } else if (animals === "dogs") {
+    return "I like dogs!";
+  }
+  return "I do not like animals at all!";
+}
