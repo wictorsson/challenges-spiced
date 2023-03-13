3de36336e058cf1dca79db31da9f56dd5ade8621
Fredrik Wictorsson <wictorsson@gmail.com>
2023-03-13 16:40:32 +0100

    solvepeople

---
+++ js-unit-testing_people/index.js
@@ -0,0 +1,45 @@
+export function getFirstNames(people) {
+  return people.map((person) => person.firstName);
+}
+
+export function getFullNames(people) {
+  return people.map((person) => person.firstName + " " + person.lastName);
+}
+
+export function getNameAndAge(people) {
+  return people.map((person) => person.lastName + " (" + person.age + ")");
+}
+
+export function getPeopleByAge(people, age) {
+  return people.filter((person) => person.age === age);
+}
+
+// export function getPeopleByAge(people, age) {
+//   people.filter((person) => {
+//     return person.age === age;
+//   });
+// }
+
+export function getPeopleNamesOlderThan(people, age) {
+  return people
+    .filter((person) => person.age > age)
+    .map((person) => person.firstName + " " + person.lastName);
+}
+
+export function getPeopleByLastName(people, lastName) {
+  return people.filter((person) => person.lastName === lastName);
+}
+
+export function findPersonById(people, id) {
+  return people.find((person) => person.id === id);
+}
+
+export function isAnyoneOlderThan(people, age) {
+  return people.some((person) => person.age > 30);
+}
+
+export function getFullNamesSortedByAge(people) {
+  return people
+    .sort((a, b) => b.age - a.age)
+    .map((person) => person.firstName + " " + person.lastName);
+}
